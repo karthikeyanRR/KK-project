36976264aa3d401af164c34c7963d54336669b0a
karthikeyanRR <karthikeyankarthikeyan777@gmail.com>
2022-04-05 10:39:44 +0530

    new pojo added

---
+++ src/test/java/com/pojo/Endpojo.java
@@ -0,0 +1,15 @@
+package com.pojo;
+
+import org.openqa.selenium.WebElement;
+
+public class Endpojo {
+	public Endpojo() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	private WebElement txt;
+
+	public WebElement getTxt() {
+		return txt;
+	}
+}
